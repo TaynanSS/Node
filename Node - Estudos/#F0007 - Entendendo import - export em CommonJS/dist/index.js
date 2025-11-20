f0377bd424430b7e7a6e7136e77a275673192a7c
TaynanSS <taynansilvass2@gmail.com>
2025-11-20 15:57:40 -0300

    Adicionando Aula #F0007

---
+++ Node - Estudos/#F0007 - Entendendo import - export em CommonJS/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome = 'Taynan';
+let idade = 27;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
+//# sourceMappingURL=index.js.map
