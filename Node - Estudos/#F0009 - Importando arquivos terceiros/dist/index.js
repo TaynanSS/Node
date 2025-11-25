a00be9c4d029bf499925cd82dd6842cdd87cbea6
TaynanSS <taynansilvass2@gmail.com>
2025-11-25 15:00:02 -0300

    Aula 9 last att

---
+++ Node - Estudos/#F0009 - Importando arquivos terceiros/dist/index.js
@@ -0,0 +1,6 @@
+// import * as Matematica from ""./Matematica.js";"
+import validator from 'validator';
+console.log(validator.isEmail('emailvalido@gmail.com')); // --- Caso o e-mail seja válido, irá dar TRUE
+let ip = '127.0.0.1';
+console.log(validator.isIP(ip)); // -- Da mesma forma, irá checar se é um e-mail válido, caso seja retorna TRUE
+//# sourceMappingURL=index.js.map
---
+++ Node - Estudos/#F0009 - Importando arquivos terceiros/src/index.ts
@@ -0,0 +1,10 @@
+// import * as Matematica from ""./Matematica.js";"
+
+import validator from 'validator'
+
+
+console.log( validator.isEmail('emailvalido@gmail.com') ); // --- Caso o e-mail seja válido, irá dar TRUE
+
+let ip = '127.0.0.1';
+
+console.log(validator.isIP(ip)) // -- Da mesma forma, irá checar se é um e-mail válido, caso seja retorna TRUE
