02c84e0d3c87d4ceaa11b55eac78925828aa78fc
TaynanSS <taynansilvass2@gmail.com>
2025-11-26 23:16:35 -0300

    Aula 11 last att

--- Node - Estudos/#F0011 - Usando Nodemon com TypeScript/src/index.ts
+++ Node - Estudos/#F0011 - Usando Nodemon com TypeScript/src/index.ts
@@ -1,12 +1,20 @@
-// import * as Matematica from ""./Matematica.js";"
+// instalei a biblioteca ts-node com comando : npm install -g ts-node
+// Ela faz o processo de conversão para typescript e já executa o que está no src.
+
+// nodemon src/index.ts 
+// O comando acima irá rodar a biblioteca Nodemon, e ela automaticamente já mostra o resultado do comando
+
+// adicionei o comando "start-dev" no package.json substituiu o comando "nodemon src/index.ts"
+// basta executar o comando "npm run start-dev"
 
 import validator from 'validator'
 
 
-let name: string = 'taynan'
+let name: string = 'pedro'
 
 if(validator.isLowercase(name)) {
     console.log(`A string ${name} é toda minúscula.`)
 } else {
     console.log(`A string ${name} NÃO é toda minúscula.`)
 }
+
