e7f9bc07637fe45ec499b8e168ca5c31daf3c46d
TaynanSS <taynansilvass2@gmail.com>
2025-12-01 13:55:04 -0300

    Aula 13 last att

--- Node - Estudos/#F0013 - Teoria das rotas/src/server.ts
+++ Node - Estudos/#F0013 - Teoria das rotas/src/server.ts
@@ -6,5 +6,8 @@ const server = express();
 server.get('/', (req: Request, res: Response)=>{
     res.send('Olá, Mundo');
 });
+server.get('/contato', (req: Request, res: Response)=>{
+    res.send('Está é a página de contato.')
+})
 
 server.listen(80);
