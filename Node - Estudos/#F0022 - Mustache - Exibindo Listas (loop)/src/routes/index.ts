7107367d59ce1724cd8c5d7b00f4f109de2960fe
TaynanSS <taynansilvass2@gmail.com>
2025-12-04 10:04:27 -0300

    Aula 22 last att

---
+++ Node - Estudos/#F0022 - Mustache - Exibindo Listas (loop)/src/routes/index.ts
@@ -0,0 +1,38 @@
+import { Router, type Request, type Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response)=>{
+    let age: number = 90;
+    let showOld: boolean = false
+    if(age > 50) {
+        showOld = true;
+    } 
+    
+    res.render('home', {
+        name: 'Taynan',
+        lastName: 'Silva',
+        showOld,
+        products: [
+            {title: 'Produto X', price: 10},
+            {title: 'Produto Y', price: 15},
+            {title: 'Produto Z', price: 20}
+        ],
+        frasesDoDia: [
+            'Alguma coisa muito legal',
+            'Outra frase qualquer'
+        ]
+    })
+})
+router.get('/contato', (req: Request, res: Response)=>{
+
+    res.send('Formulário de Contato');
+})
+router.get('/sobre', (req: Request, res: Response)=>{
+
+    res.send('Página Sobre a empresa');
+})
+
+
+
+export default router;
