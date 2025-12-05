8121f00296b0aa76800fd87a1d7d6181663648ee
TaynanSS <taynansilvass2@gmail.com>
2025-12-05 12:28:18 -0300

    Aula 24 last att

---
+++ Node - Estudos/#F0024 - Recebendo dados na URL 1/src/routes/index.ts
@@ -0,0 +1,46 @@
+import { Router, type Request, type Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response)=>{
+    let age: number = 90;
+    let showOld: boolean = false
+    if(age > 50) {
+        showOld = true;
+    } 
+    
+    res.render('pages/home', {
+        name: 'Taynan',
+        lastName: 'Silva',
+        showOld,
+        products: [
+            {title: 'Produto X', price: 10},
+            {title: 'Produto Y', price: 15},
+            {title: 'Produto Z', price: 20}
+        ],
+        frasesDoDia: [
+            'Alguma coisa muito legal',
+            'Outra frase qualquer'
+        ]
+    })
+})
+router.get('/contato', (req: Request, res: Response)=>{
+
+    res.render('pages/contato')
+})
+router.get('/sobre', (req: Request, res: Response)=>{
+
+    res.render('pages/sobre')
+})
+
+router.get('/nome', (req: Request, res: Response)=>{
+    console.log('query string', req.query); // Vai mostrar no terminal o que está dentro da QUERY
+
+    let nome: string = req.query.nome as string;
+
+    res.render('pages/nome', {
+        nome
+    });
+})
+
+export default router;
